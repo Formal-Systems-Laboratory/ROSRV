a1569afdfbd6de3d881743b5dd9c2364eab5fd1f
Brandon Moore <brandon.moore@runtimeverification.com>
2020-03-12 15:21:30 -0500

    Reorder method bind list to match ROS documentation
    The XMLRPC method bindings are now set up in the
    same order that the methods are currently(2020-03-12)
    listed on the wiki page http://wiki.ros.org/ROS/Master_API

--- src/RVMaster/src/main.cpp
+++ src/RVMaster/src/main.cpp
@@ -34,36 +34,36 @@ int main(int argc, char **argv)
 
 
   //--- ros master api
+  // Order follows http://wiki.ros.org/ROS/Master_API
 
-  //subscriber and publisher
+  // register/unregister methods
+  xmlrpc_manager_->bind("registerService", boost::bind(&rv::ServerManager::registerServiceCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("unregisterService", boost::bind(&rv::ServerManager::unregisterServiceCallback, server_manager_,_1,_2,_3));
+
   xmlrpc_manager_->bind("registerSubscriber",boost::bind(&rv::ServerManager::registerSubscriberCallback,server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("unregisterSubscriber",boost::bind(&rv::ServerManager::unregisterSubscriberCallback,server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("registerPublisher",boost::bind(&rv::ServerManager::registerPublisherCallback,server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("unregisterPublisher",boost::bind(&rv::ServerManager::unregisterPublisherCallback,server_manager_,_1,_2,_3));
-  //xmlrpc_manager_->bind("publisherUpdate", boost::bind(&rv::ServerManager::pubUpdateCallback, server_manager_, _1, _2,_3));
 
-  //other commands
+  // Name servic and system state
+  //subscriber and publisher
   xmlrpc_manager_->bind("lookupNode", boost::bind(&rv::ServerManager::lookupNodeCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("getSystemState", boost::bind(&rv::ServerManager::getSystemStateCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("getPid", boost::bind(&rv::ServerManager::getPidCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("getUri", boost::bind(&rv::ServerManager::getUriCallback, server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("getPublishedTopics", boost::bind(&rv::ServerManager::getPublishedTopicsCallback, server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("getTopicTypes", boost::bind(&rv::ServerManager::getTopicTypesCallback, server_manager_,_1,_2,_3));
-
-  //service
-  xmlrpc_manager_->bind("registerService", boost::bind(&rv::ServerManager::registerServiceCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("unregisterService", boost::bind(&rv::ServerManager::unregisterServiceCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("getSystemState", boost::bind(&rv::ServerManager::getSystemStateCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("getUri", boost::bind(&rv::ServerManager::getUriCallback, server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("lookupService", boost::bind(&rv::ServerManager::lookupServiceCallback, server_manager_,_1,_2,_3));
 
-  //parameter server
-  xmlrpc_manager_->bind("unsubscribeParam", boost::bind(&rv::ServerManager::unsubscribeParamCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("subscribeParam", boost::bind(&rv::ServerManager::subscribeParamCallback, server_manager_,_1,_2,_3));
+  // --- parameter server api
+  // Order follows http://wiki.ros.org/ROS/Parameter%20Server%20API
   xmlrpc_manager_->bind("deleteParam", boost::bind(&rv::ServerManager::deleteParamCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("getParam", boost::bind(&rv::ServerManager::getParamCallback, server_manager_,_1,_2,_3));
-  xmlrpc_manager_->bind("getParamNames", boost::bind(&rv::ServerManager::getParamNamesCallback, server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("setParam", boost::bind(&rv::ServerManager::setParamCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("getParam", boost::bind(&rv::ServerManager::getParamCallback, server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("searchParam", boost::bind(&rv::ServerManager::searchParamCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("subscribeParam", boost::bind(&rv::ServerManager::subscribeParamCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("unsubscribeParam", boost::bind(&rv::ServerManager::unsubscribeParamCallback, server_manager_,_1,_2,_3));
   xmlrpc_manager_->bind("hasParam", boost::bind(&rv::ServerManager::hasParamCallback, server_manager_,_1,_2,_3));
+  xmlrpc_manager_->bind("getParamNames", boost::bind(&rv::ServerManager::getParamNamesCallback, server_manager_,_1,_2,_3));
 
   xmlrpc_manager_->start();
   server_manager_->start();
